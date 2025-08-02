63c9374c270412058c002538ab18d575e32239f3
Vaibhav Sutar <sutar.vaibhav649@gmail.com>
2025-08-02 09:38:06 +0530

    change

--- app/utils/prompt_template.py
+++ app/utils/prompt_template.py
@@ -1,7 +1,7 @@
 def generate_prompt(query: str, context_chunks: list[str]) -> str:
     context = "\n---\n".join(context_chunks)
     return f"""
-You are a policy assistant. Use only the below context to answer the query.
+    You are a policy assistant. Use only the below context to answer the query.
 
 Context:
 {context}
